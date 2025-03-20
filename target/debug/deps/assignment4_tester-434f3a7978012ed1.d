59e3f765117b8c803c59b81807a7337bbfdc30f0
Dor <dorsa@post.jce.ac.il>
2025-03-20 18:02:24 +0200

    release version

---
+++ target/debug/deps/assignment4_tester-434f3a7978012ed1.d
@@ -0,0 +1,6 @@
+/home/ubuntie/rust/shimrit/2025/assignments/target/debug/deps/assignment4_tester-434f3a7978012ed1: assignment4-tester/src/lib.rs assignment4-tester/src/hw4_tests.rs
+
+/home/ubuntie/rust/shimrit/2025/assignments/target/debug/deps/assignment4_tester-434f3a7978012ed1.d: assignment4-tester/src/lib.rs assignment4-tester/src/hw4_tests.rs
+
+assignment4-tester/src/lib.rs:
+assignment4-tester/src/hw4_tests.rs:
